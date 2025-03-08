5d6dbe1ff23fe2cb2e2ae0b69b770b80259216b6
Archford Nhanga <archfordnhanga@gmail.com>
2025-03-09 01:07:38 +0200

    exercise-2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+
+
+        int percentage = int.Parse(userInput);
+
+
+        string letter;
+        string sign = "";
+
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        int lastDigit = percentage % 10;
+
+
+        if (letter == "F")
+        {
+
+            sign = "";
+        }
+        else if (letter == "A" && lastDigit >= 7)
+        {
+
+            sign = "";
+        }
+        else
+        {
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard. You'll do better next time!");
+        }
     }
 }
